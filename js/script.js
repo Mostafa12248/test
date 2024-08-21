29858ef346afe65bd670d110b3e5d93c696f5330
Mostafa12248 <mostafayahya000@gmail.com>
2024-08-21 12:28:07 +0300

    test

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const toggleBg = document.querySelector('span');
 const container = document.querySelector('.container');
 const label = document.querySelectorAll('label');
 const input = document.querySelectorAll('input');
+const option = document.querySelector('.options p');
 
 const themesAction = () => {
   logo.classList.toggle('nav_text_color');
@@ -18,13 +19,22 @@ const themesAction = () => {
   themesP.classList.toggle('nav_text_color');
   container.classList.toggle('container_style');
   label.classList.toggle('nav_text_color');
+  option.classList.toggle('option_style');
 };
 
 themes.addEventListener('click', themesAction);
 
 for (let x = 0; x < label.length; x++) {
   input[x].addEventListener('focus', () => {
+    const test = document.querySelector('.input_focus');
+    if (test) {
+      test.classList.remove('input_focus');
+    }
+    input[x].classList.add('input_focus');
     label[x].classList.toggle('label_style');
-    input[x].classList.toggle('input_focus');
+  });
+
+  input[x].addEventListener('blur', () => {
+    label[x].classList.toggle('label_style_2');
   });
 }
