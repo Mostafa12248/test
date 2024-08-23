d9a00d2e5d06777a114bc5056eb5cc3c7d12da2e
Mostafa12248 <mostafayahya000@gmail.com>
2024-08-23 18:58:20 +0300

    finished

--- js/script.js
+++ js/script.js
@@ -1,18 +1,28 @@
 const logo = document.querySelector('.p');
 const themes = document.querySelector('.themes');
 const themesP = document.querySelector('.themes p');
-const toggle = document.querySelector('span');
+const toggle = document.querySelector('.circle');
 const nav = document.querySelector('nav');
 const body = document.querySelector('body');
 const toggleBg = document.querySelector('span');
 const container = document.querySelector('.container');
 const label = document.querySelectorAll('label');
 const input = document.querySelectorAll('input');
-const userName = document.querySelectorAll('.name');
 const option = document.querySelector('.options p');
 const correct = document.querySelectorAll('.correct');
 const correctEmail = document.querySelector('.correct_email');
 const email = document.querySelector('.email');
+const userName = document.querySelectorAll('.name');
+const pass = document.querySelector('.pass');
+const correctPass = document.querySelector('.correctPass');
+
+pass.addEventListener('input', () => {
+  if (pass.value >= '8') {
+    correctPass.style.display = 'block';
+  } else {
+    correctPass.style.display = 'none';
+  }
+});
 
 const emailEvent = (event) => {
   if (event.key === '@') {
@@ -23,13 +33,13 @@ const emailEvent = (event) => {
 };
 
 const themesAction = () => {
+  container.classList.toggle('container_style');
   logo.classList.toggle('nav_text_color');
   toggleBg.classList.toggle('nav_text_color');
   toggle.classList.toggle('toggle_style');
   nav.classList.toggle('nav_style');
   body.classList.toggle('body_style');
   themesP.classList.toggle('nav_text_color');
-  container.classList.toggle('container_style');
   label.classList.toggle('nav_text_color');
   option.classList.toggle('option_style');
 };
@@ -46,12 +56,21 @@ for (let x = 0; x < label.length; x++) {
     input[x].classList.add('input_focus');
     label[x].classList.add('label_style');
   });
+
+  input[x].addEventListener('blur', () => {
+    if (!input[x].value.trim()) {
+      input[x].classList.add('border_color');
+    } else {
+      input[x].classList.remove('border_color');
+    }
+  });
 }
 
 for (let z = 0; z < userName.length; z++) {
   userName[z].addEventListener('input', () => {
-    if (userName[z].value.trim() !== '') {
+    if (userName[z].value.trim()) {
       correct[z].style.display = 'block';
+      userName[z].classList.remove('border_color');
     } else {
       correct[z].style.display = 'none';
     }
